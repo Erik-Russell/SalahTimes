7f7a837509f8147024a6ff5caa54f231c55def63
Erik Russell <erik.z.russell@gmail.com>
2026-03-11 11:23:38 -0700

    Location class able to return lat and long

--- src/main/java/SalahTimes.java
+++ src/main/java/SalahTimes.java
@@ -5,23 +5,29 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
-import java.util.Scanner;
+import java.util.*;
+
+import com.google.gson.Gson;
 
 /**
  * SalahTimes
  */
 public class SalahTimes {
-    public static void main(String[] args)
+    static void main(String[] args)
             // IOException and InterruptedException required for HttpClient.send()
             throws IOException, InterruptedException {
         Scanner scanner = new Scanner(System.in);
         IO.println("Welcome to the SalahTimes app");
         IO.print("What city do you live in? ");
-        String userCity = scanner.next();
+        String userCity = scanner.nextLine();
         // create Location Object
         Location location = new Location(userCity);
         // use method to geocode city
         location.geolocate();
+
+        IO.println(location.toString());
+
+        // call PTC with location
     }
 }
 
@@ -29,12 +35,21 @@ public class SalahTimes {
  * Holds user's geographic data
  */
 class Location {
+    private static class Result {
+        double latitude;
+        double longitude;
+    }
+
+    private static class Response {
+        List<Result> results;
+    }
+
     public double latitude;
     public double longitude;
     public String cityName;
 
     public Location(String city){
-        IO.println("Making a new Location with: " + city);
+        IO.println("Your city: " + city);
         this.cityName = city;
     }
 
@@ -47,6 +62,9 @@ class Location {
     // geocode city name
     // IOException and InterruptedException required for HttpClient.send()
     public void geolocate() throws IOException, InterruptedException {
+
+        Gson gson = new Gson();
+
         // encoding for safe URL usage
         String encodedCity = URLEncoder.encode(this.cityName, StandardCharsets.UTF_8);
 
@@ -56,6 +74,7 @@ class Location {
 
         // create HttpClient
         try (HttpClient client = HttpClient.newHttpClient()) {
+            IO.println("Getting coordinates of " + this.cityName + "...");
 
             // build Http GET request for API
             HttpRequest request = HttpRequest.newBuilder()
@@ -69,9 +88,10 @@ class Location {
 
             // assign results
             if (response.statusCode() == 200) {
-                //DEBUG
-                IO.println("DEBUG: response code == 200");
-                IO.println(response.body());
+                Response responseJson = gson.fromJson(response.body(), Response.class);
+
+                this.latitude = responseJson.results.getFirst().latitude;
+                this.longitude = responseJson.results.getFirst().longitude;
             } else {
                 IO.println("ERR: Bad STATUS CODE");
             }
