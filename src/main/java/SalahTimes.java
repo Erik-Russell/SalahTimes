83295cd14d31085413e40d84218134ded2969648
Erik Russell <erik.z.russell@gmail.com>
2026-03-12 19:06:55 -0700

    Corrected formulas and now have getSunrise() functioning

--- src/main/java/SalahTimes.java
+++ src/main/java/SalahTimes.java
@@ -5,6 +5,8 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.*;
 
 import com.google.gson.Gson;
@@ -25,9 +27,11 @@ public class SalahTimes {
         // use method to geocode city
         location.geolocate();
 
-        IO.println(location.toString());
+        //IO.println(location.toString());
 
         PrayerTimesCalculator prayerTimesCalculator = new PrayerTimesCalculator(location);
+
+        prayerTimesCalculator.calculateSolarPositions();
     }
 }
 
@@ -129,16 +133,30 @@ class Location {
  * Applies prayer time formulas to solar data
  */
 class PrayerTimesCalculator {
-    public double fajrAngle;
-    public double ishaAngle;
+    public double fajrAngle = -15.0;
+    public double ishaAngle = -15.0;
     public Location location;
+    public SolarCalculator solarCalc;
 
     public PrayerTimesCalculator(Location location){
         this.location = location;
         IO.println("Made a PrayerTimesCalculator with location:\n" + this.location);
+        this.solarCalc = new SolarCalculator(this.location);
     }
 
-    SolarCalculator solarCalc = new SolarCalculator(this.location);
+    public void calculateSolarPositions(){
+        double sunriseTime = this.solarCalc.getSunrise();
+        IO.println("sunrise: " + sunriseTime);
+    }
+
+    @Override
+    public String toString() {
+        return "Prayer Times Calculator\n" +
+                "fajrAngle: " + this.fajrAngle +
+                "\nishaAngle: " + this.ishaAngle +
+                "\nlocation: " + this.location +
+                "\nSolarCalculator: " + this.solarCalc;
+    }
 }
 
 /**
@@ -149,15 +167,42 @@ class SolarCalculator {
     public Calendar localDate = new GregorianCalendar();
     public int dayOfYear;
     public int hour;
+
     public SolarCalculator(Location location){
         this.location = location;
-        localDate.setTime(new Date());
-        dayOfYear = localDate.get(Calendar.DAY_OF_YEAR);
-        hour = localDate.get(Calendar.HOUR);
+        this.localDate.setTime(new Date());
+        this.dayOfYear = localDate.get(Calendar.DAY_OF_YEAR);
+        this.hour = localDate.get(Calendar.HOUR);
     }
 
-    public double getSunrise(Location location){
+    public double getSolarZenith(){
+        double y = fractionalYear(this.localDate);
+        double eqtime = eqTime(y);
+        double declination = decl(y);
+        double timeOffset = timeOffset(eqtime, this.location, this.location.timezone);
+
         return 0.0;
+    }
+
+    public double getSunrise() {
+        double y = fractionalYear(this.localDate);
+        double eqtime = eqTime(y);
+        double declination = decl(y);
+        double latRadian = Math.toRadians(this.location.getLatitude());
+
+        double hourAngle = Math.acos(Math.cos(Math.toRadians(90.833))
+                / (Math.cos(latRadian) * Math.cos(declination))
+                - (Math.tan(latRadian) * Math.tan(declination)));
+
+        IO.println("DEBUG: hourAngle = " + hourAngle);
+
+        // convert to degrees for timeOfSunrise calculation
+        hourAngle = Math.toDegrees(hourAngle);
+
+        // in minutes
+        double utcTimeOfSunrise = 720 - 4 * (this.location.getLongitude() + hourAngle) - eqtime;
+
+        return utcTimeOfSunrise / 60;
     }
 
     private double fractionalYear(Calendar localDate){
@@ -178,8 +223,8 @@ class SolarCalculator {
                 (0.000075 +
                         (0.001868 * Math.cos(y)) -
                         (0.032077 * Math.sin(y)) -
-                        (0.014615 * Math.cos(2 * y) -
-                                (0.040849 * Math.sin(2 * y))));
+                        (0.014615 * Math.cos(2 * y)) -
+                                (0.040849 * Math.sin(2 * y)));
         IO.println("DEBUG: equation of time = " + eqTime);
         return eqTime;
     }
@@ -195,8 +240,27 @@ class SolarCalculator {
                 (0.000907 * Math.sin(2 * y)) -
                 (0.002697 * Math.cos(3 * y)) +
                 (0.00148 * Math.sin(3 * y));
-        IO.println("DEBUG: equation of time = " + decl);
+        IO.println("DEBUG: declination = " + decl);
         return decl;
+    }
+
+    public double timeOffset(double eqTime, Location location, String timezone){
+        // convert timezone name to number offset
+        ZoneId zone = ZoneId.of(timezone);
+        double offset = (double) ZonedDateTime.now(zone).getOffset().getTotalSeconds() / 3600;
+
+        double timeOffset = eqTime + 4 * location.longitude - 60 * offset;
+        IO.println("DEBUG: time_offset = " + timeOffset);
+        return timeOffset;
+    }
+
+    public double trueSolarTime(){ return 0.0; }
+
+    public String toString() {
+        return "\nlocation: " + this.location +
+        "\ndate: " + this.localDate +
+        "\nday: " + this.dayOfYear +
+        "\nhour: " + this.hour;
     }
 }
 
